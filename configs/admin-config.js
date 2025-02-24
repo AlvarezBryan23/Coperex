19de7badab655e4f7644c60b08007b8900d73b88
AlvarezBryan23 <balvarez-2023244@kinal.edu.gt>
2025-02-24 17:24:24 -0600

    Se esta realizando el login

---
+++ configs/admin-config.js
@@ -0,0 +1,4 @@
+export const adminConfig = {
+    email: "",
+    password: ""
+};
--- src/auth/auth.routes.js
+++ src/auth/auth.routes.js
@@ -1,11 +1,9 @@
 import { Router } from "express";
-import { register, login } from "./auth-controller.js";
-import { registerValidator, loginValidator} from "../middlewares/check-validator.js"
+import { login } from "./auth-controller.js";
+import { loginValidator } from "../middlewares/check-validator.js";
 
-const router = Router()
+const router = Router();
 
-router.post("/register", registerValidator, register)
+router.post("/login", loginValidator, login);
 
-router.post("/login", loginValidator, login)
-
-export default router
+export default router;
